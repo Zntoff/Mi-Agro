088390835f7ba097aa11d31631a57b2c0870c08c
Edison Ramirez <oficial.edisonra@gmail.com>
2022-09-29 11:52:30 -0500

    Update

--- src/main/java/beans/Product.java
+++ src/main/java/beans/Product.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package beans;
 
-/**
- *
- * @author ofici
- */
+import java.util.Date;
+
 public class Product {
-    
+    private int Idproduct ;
+    private String titulo;
+    private Date fechaAlquiler;
+    private boolean novedad;
+    private String genero;
 }
